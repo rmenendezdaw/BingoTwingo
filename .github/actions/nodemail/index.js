8b12197d13ad2fe5d2a2c36e322370bf3762384e
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-01 18:50:50 +0100

    Repare message email 2

--- .github/actions/nodemail/index.js
+++ .github/actions/nodemail/index.js
@@ -3,11 +3,16 @@ const core = require("@actions/core");
 
 const user_email = core.getInput("user_email");
 const email = core.getInput("email");
-const password = core.getInput("pass");
-const syntax_check_job = core.getInput("syntax_check_job");
-const test_execution_job = core.getInput("test_execution_job");
-const build_statics_job = core.getInput("build_statics_job");
-const deploy_job = core.getInput("deploy_job");
+const password = core.getInput("pass")
+let syntax_check_job
+let test_execution_job
+let build_statics_job
+let deploy_job
+
+core.getInput("syntax_check_job") == 'success' ? syntax_check_job = 'success' : syntax_check_job = "error";
+core.getInput("test_execution_job") == 'success' ? test_execution_job = 'success' : test_execution_job = "error";
+core.getInput("build_statics_job") == 'success' ? build_statics_job = 'success' : build_statics_job = "error";
+core.getInput("deploy_job") == 'success' ? deploy_job = 'success' : deploy_job = "error";
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -23,10 +28,10 @@ var mailOptions = {
   subject: 'Resultado del workflow ejecutado',
   text:` Se ha realizado un push en la rama githubActions_improvement 
   que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados: \n
-  \n test_execution_job: ${{test_execution_job}}
-  \n syntax_check_job: ${{syntax_check_job}}
-  \n build_statics_job: ${{build_statics_job}}
-  \n deploy_job: ${{deploy_job}}`,
+  \n test_execution_job: ${test_execution_job}
+  \n syntax_check_job: ${syntax_check_job}
+  \n build_statics_job: ${build_statics_job}
+  \n deploy_job: ${deploy_job}`,
 };
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
