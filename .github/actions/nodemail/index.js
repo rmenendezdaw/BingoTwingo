5b76409b4d78b2d590202e04da91035ce6c871ee
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-01 15:43:33 +0100

    Prueba 5-1

---
+++ .github/actions/nodemail/index.js
@@ -0,0 +1,38 @@
+var nodemailer = require('nodemailer');
+const core = require("@actions/core");
+
+const user_email = core.getInput("user_email");
+const email = core.getInput("email");
+const pass = core.getInput("pass");
+const syntax_check_job = core.getInput("syntax_check_job");
+const test_execution_job = core.getInput("test_execution_job");
+const build_statics_job = core.getInput("build_statics_job");
+const deploy_job = core.getInput("deploy_job");
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: pass
+  }
+});
+
+var mailOptions = {
+  from: email,
+  to: user_email,
+  subject: 'Resultado del workflow ejecutado',
+  text:` Se ha realizado un push en la rama githubActions_improvement 
+  que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados: \n
+  \n test_execution_job: ${check_skipped(test_execution_job)}
+  \n syntax_check_job: ${check_skipped(syntax_check_job)}
+  \n build_statics_job:${check_skipped(build_statics_job)}
+  \n deploy_job: ${check_skipped(deploy_job)}`,
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
